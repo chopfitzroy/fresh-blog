6614b68c49b50dc5a74d5c6f2154186c99167035
Otis Sutton <chop@DESKTOP-5NVK6P0.localdomain>
2022-12-17 16:01:59 +1300

    List blog pages

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,26 +1,79 @@
-import { Head } from "$fresh/runtime.ts";
-import Counter from "../islands/Counter.tsx";
-import Markdown from "https://esm.sh/markdown-to-jsx@7.1.8?alias=react:preact/compat&deps=preact@10.8.2";
+import { join } from "$std/path/mod.ts";
+import { Handlers } from "$fresh/server.ts";
+import { PageProps } from "$fresh/server.ts";
+import { extract } from "$std/encoding/front_matter.ts";
 
-export default function Home() {
+interface Post {
+  slug: string;
+  title: string;
+  publishedAt: Date;
+  content: string;
+  snippet: string;
+}
+
+export const handler: Handlers<Post[]> = {
+  async GET(_req, ctx) {
+    const posts = await getPosts();
+    return ctx.render(posts);
+  },
+};
+
+async function getPost(slug: string): Promise<Post | null> {
+  const text = await Deno.readTextFile(join("./posts", `${slug}.md`));
+  const { attrs, body } = extract(text);
+  return {
+    slug,
+    title: attrs.title,
+    publishedAt: new Date(attrs.published_at),
+    content: body,
+    snippet: attrs.snippet,
+  };
+}
+
+async function getPosts(): Promise<Post[]> {
+  const files = Deno.readDir("./posts");
+  const promises = [];
+  for await (const file of files) {
+    const slug = file.name.replace(".md", "");
+    promises.push(getPost(slug));
+  }
+  const posts = await Promise.all(promises) as Post[];
+  posts.sort((a, b) => b.publishedAt.getTime() - a.publishedAt.getTime());
+  return posts;
+}
+
+function PostCard(props: { post: Post }) {
+  const { post } = props;
   return (
-    <>
-      <Head>
-        <title>Fresh App</title>
-      </Head>
-      <div class="p-4 mx-auto max-w-screen-md">
-        <Markdown>### Hello there old chap!</Markdown>
-        <img
-          src="/logo.svg"
-          class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-          file, and refresh.
-        </p>
-        <Counter start={3} />
-      </div>
-    </>
+    <div class="py-8 border(t gray-200)">
+      <a class="sm:col-span-2" href={`/${post.slug}`}>
+        <h3 class="text(3xl gray-900) font-bold">
+          {post.title}
+        </h3>
+        <time class="text-gray-500">
+          {new Date(post.publishedAt).toLocaleDateString("en-us", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+        </time>
+        <div class="mt-4 text-gray-900">
+          {post.snippet}
+        </div>
+      </a>
+    </div>
   );
 }
+
+export default function BlogIndexPage(props: PageProps<Post[]>) {
+  const posts = props.data;
+  return (
+    <main class="max-w-screen-md px-4 pt-16 mx-auto">
+      <h1 class="text-5xl font-bold">Blog</h1>
+      <div class="mt-8">
+        {posts.map((post) => <PostCard post={post} />)}
+      </div>
+    </main>
+  );
+}
+
